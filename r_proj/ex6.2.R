58b679f8d5e34f646881f9c4df6067bbf8165c3e
samantoz <samantoz@gmail.com>
2021-11-10 23:11:09 -0600

    added from laptop

---
+++ r_proj/ex6.2.R
@@ -0,0 +1,34 @@
+## 1 histogram, 1 box plot, 1 bullet chart, and 1 additional chart of your choice 
+## (can be an existing chart type we've already done, but it must be done in a new way 
+## or it can be an entirely new chart type) using R
+
+# Crime rate in the US
+crime <- read.csv("C:\\Users\\saman\\git_repos\\dsc640-data_presentation\\data\\ex6.2\\crimeratesbystate-formatted.csv",header=TRUE)
+# Education data
+
+# Lets look at the first 5 rows
+education <- read.csv("C:\\Users\\saman\\git_repos\\dsc640-data_presentation\\data\\ex6.2\\education.csv",header=TRUE)
+
+education[1:5,]
+
+# Extra plot
+# Looking at the scores for various states
+ed.dis <- dist(education[,2:7])
+ed.mds <- cmdscale(ed.dis)
+
+x <- ed.mds[,1]
+y <- ed.mds[,2]
+
+plot(x,y, type="n")
+text(x, y, labels=education$state)
+
+# Histogram
+hist(crime$burglary, prob=F, ylim=c(0,15), 
+     main="Histogram of Burglary rate by State in the USA", xlab="burglary",ylab = "# of States", las=1)
+
+# Boxplot
+boxplot(crime$burglary, crime$robbery, col="yellow",
+        main="Boxplot showing burglary and robbery")
+
+# Bullet Chart
+bullet_chart(dataframe = education)
